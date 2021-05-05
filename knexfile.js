50a99616f76713fa9768effb1585b9083efcd497
Matheo <math.sou@hotmail.com>
2021-05-05 20:23:33 -0300

    criando ambiente para o heroku

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
 // Update with your config settings.
-
+require('dotenv').config();
 module.exports = {
 
   development: {
@@ -32,11 +32,12 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'mysql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+        host : process.env.DATABASE_HOST,
+        user : process.env.DATABASE_USER,
+        password : process.env.DATABASE_PASSWORD,
+        database : process.env.DATABASE_DB
     },
     pool: {
       min: 2,
--- src/database/migrations/20210412130547_create_user.js
+++ src/database/migrations/20210412130547_create_user.js
@@ -5,7 +5,14 @@ exports.up = function(knex) {
       table.string('name').notNullable();
       table.string('email').notNullable();
       table.string('password').notNullable();
-
+      table.string('city_id').notNullable();
+      table.string('facebook');
+      table.string('instagram');
+      table.boolean('showFacebook').notNullable();
+      table.boolean('showInstagram').notNullable();
+      table.boolean('showEmail').notNullable();
+      
+      table.foreign('city_id').references('city.id').onDelete('CASCADE').onUpdate('CASCADE');
     });
   };
   
