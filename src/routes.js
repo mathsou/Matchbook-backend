352fa8c2883b7ff9115e1a024cb4427abaa3c892
Matheo <math.sou@hotmail.com>
2021-06-08 23:33:43 -0300

    configurado rotas

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require ('express');
 const connection = require('./database/connection');
 const userController = require('./controllers/userController');
 const sessionController = require('./controllers/sessionController');
+const homeController = require('./controllers/homeController');
 const bookController = require('./controllers/bookController');
 const profileController = require('./controllers/profileController');
 
@@ -17,12 +18,16 @@ routes.post('/login', sessionController.create);
 
 routes.use(authMiddleware);
 
-routes.get('/book', bookController.index);
+routes.get('/home', homeController.index);
+
 routes.get('/user', userController.index);
 routes.put('/user', userController.modify);
 
 routes.get('/profile', profileController.index);
 routes.post('/profile', profileController.create);
 routes.put('/profile', profileController.modify);
+
+routes.get('/book', bookController.index);
+routes.post('/book', bookController.create);
 
 module.exports = routes;
