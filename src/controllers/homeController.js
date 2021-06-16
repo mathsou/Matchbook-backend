680298af20ff49830ebe5118d4e6edc4114d1da8
Matheo <math.sou@hotmail.com>
2021-06-16 15:35:47 -0300

    corrigido função de likes

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -5,7 +5,7 @@ module.exports = {
         const user_id = response.locals.id;
         //NOT READY
         const books = await connection('book')
-        .select(["book.id", "book.name as book", "book.author", "user.id as user_id","user.name"])
+        .select(["book.id", "book.name as book", "book.author", "user.id as user_owner","user.name"])
         .join('user', 'user.id', '=', 'book.user_id')
         .where('book.user_id', "!=",user_id.id)
         .whereNotIn("book.id",
@@ -20,15 +20,15 @@ module.exports = {
     },
 
     async create(request, response){
-        const id = response.locals.id;
-        const {book_id, liked, user_id} = request.body;
+        const user_id = response.locals.id;
+        const {book_id, liked, user_owner} = request.body;
 
         if(typeof(book_id) === "number" || typeof(liked) === "boolean"){
             await connection('likes')
             .insert({
                 liked,
                 book_id,
-                user_id: id.id
+                user_id: user_id.id
             });
            
             return response.json({"success": true, "status": 0, "message": "Success"});
