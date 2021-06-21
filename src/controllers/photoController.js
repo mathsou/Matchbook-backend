8191cdb0bf974c644cad83bfa598a6a751f06eab
Matheo <math.sou@hotmail.com>
2021-06-21 20:57:03 -0300

    finalizado multer

--- src/controllers/photoController.js
+++ src/controllers/photoController.js
@@ -2,13 +2,13 @@ const connection = require('../database/connection');
 
 module.exports = {
     async create(request, response){
-        const { originalname: name, size, filename: photo, location: url = "" } = request.file;
+        const { originalname: name, size, key: photo, location: url = "" } = request.file;
 
         const photos = await {
             photo,
             size,
             url
-        }
+        };
 
 
          return response.json({"success": true, "status": 0, "message": "Success", "data":{photos}});
