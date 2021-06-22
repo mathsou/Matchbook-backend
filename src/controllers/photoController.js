22ac37e92c08902f0396ff3de5a39eaf60f32c41
Matheo <math.sou@hotmail.com>
2021-06-21 21:06:37 -0300

    corrigingo merge

--- src/controllers/photoController.js
+++ src/controllers/photoController.js
@@ -1,14 +1,21 @@
 const connection = require('../database/connection');
 
 module.exports = {
+    async index(request, response){
+        return true;
+    },
     async create(request, response){
-        const { originalname: name, size, key: photo, location: url = "" } = request.file;
-
-        const photos = await {
-            photo,
-            size,
-            url
-        };
+        const {book_id} = request.params;
+        const { filename: photo, size, location: url = "" } = request.file;
+        const user_id = response.locals.id;
+        const userId = await connection('book').select('user_id').where('id', book_id);
+        console.log(user_id, userId);
+        await connection('photos')
+            .insert({
+                photo,
+                size,
+                book_id
+            });
 
 
          return response.json({"success": true, "status": 0, "message": "Success", "data":{photos}});
