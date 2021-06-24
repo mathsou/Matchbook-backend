9bfb96e826925860ec298fac4a43b9f3db511022
Matheo <math.sou@hotmail.com>
2021-06-23 21:57:01 -0300

    requisição de um livro

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -3,15 +3,32 @@ const connection = require('../database/connection');
 module.exports = {
     async index(request, response){
         const user_id = response.locals.id;
-        const books = await connection('book')
-        .select([
-            'book.id',
-            'book.name',
-            'book.author',
-            'book.category'       
-        ])
-        .join('user', 'user.id', '=', 'book.user_id')
-        .where('user_id', user_id.id);
+        const {book_id} = request.params;
+        if(book_id){
+            const books = await connection('book')
+            .select([
+                'book.id',
+                'book.name',
+                'book.author',
+                'book.category'       
+            ])
+            .join('user', 'user.id', '=', 'book.user_id')
+            .where('user_id', user_id.id)
+            .andWhere('book.id', book_id)
+            .first()
+            }else{
+                const books = await connection('book')
+                .select([
+                    'book.id',
+                    'book.name',
+                    'book.author',
+                    'book.category',
+                    'user.city'      
+                ])
+                .join('user', 'user.id', '=', 'book.user_id')
+                .where('user_id', user_id.id);
+
+            }
 
         return response.json({"success": true, "status": 0, "message": "Success", "data": books});
     },
